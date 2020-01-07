ada7e8520e2185e0fe51d31860b13aa99126cff9
Adam Payne <ajpayne238@gmail.com>
2020-01-07 16:07:53 -0500

    added couch function to resumeReview.js

--- src/resolvers/ResumeReview.js
+++ src/resolvers/ResumeReview.js
@@ -4,6 +4,11 @@ function __resolveReference(resumeReview, context) {
 	return context.prisma.resumeReview({ id: resumeReview.id })
 }
 
+function coach(resumeReview) {
+    return { __typename: 'User', id: resumeReview.coachID }
+}
+
 module.exports = {
     __resolveReference,
+    coach,
 }
--- src/resolvers/ReviewerListing.js
+++ src/resolvers/ReviewerListing.js
@@ -1,12 +1,12 @@
 
-function coach(reviewerListing) {
-    return { __typename: 'User', id: reviewerListing.coachID }
-}
 
 function __resolveReference(reviewerListing, context) {
     return context.prisma.reviewerListing({ id: reviewerListing.id })
 }
 
+function coach(reviewerListing) {
+    return { __typename: 'User', id: reviewerListing.coachID }
+}
 
 module.exports = {
     __resolveReference,
