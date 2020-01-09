9e51eeaf1d55bdef8250ebadd37f261b6d66f9a4
Aaron Clark <aaronpclark92@gmail.com>
2020-01-09 15:31:59 -0600

    rm __resolveReference from User.js

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -2,9 +2,7 @@ function reviewerListing(parent, _args, context) {
   return context.prisma.reviewerListing({ coachID: parent.id })
 }
 
-// function __resolveReference(reviewerListing, context) {
-//   return context.prisma.reviewerListing({ id: reviewerListing.id })
-// }
+
 
 module.exports = {
   reviewerListing,
