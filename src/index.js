20dc1a46e40d1d0761cd225c6c74787baf2a8279
Aaron Clark <aaronpclark92@gmail.com>
2020-01-27 12:11:08 -0600

    updated docker-compose and prisma.yml for local deployment on /resumeq service, pointed options.port to APOLLO_PORT env

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 
 const server = require('./server')
-const options = { port: process.env.PORT || 4000 }
+const options = { port: process.env.APOLLO_PORT || 4000 }
 
 server.start(options, ({ port }) => {
     console.log(`No Errors On ${port}`)
