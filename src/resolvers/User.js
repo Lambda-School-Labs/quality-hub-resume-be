5860dca3623df391315e73b428c91271d314c431
Aaron Clark <aaronpclark92@gmail.com>
2020-02-01 12:37:28 -0600

    removed __resolveReference from User resolver to stop conflict when retrieving reviewerListing for a User

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,3 +1,4 @@
+
 function reviewerListing(parent, _args, context) {
   return context.prisma.reviewerListing({ coachID: parent.id })
 }
@@ -10,14 +11,10 @@ function seeker_resume_reviews(parent, _args, context) {
   return context.prisma.resumeReviews({ where: { seeker: parent.id } })
 }
 
-function __resolveReference(parent, args, { prisma }) {
-  const res = prisma.user({ id: parent.id })
-  return res
-}
+
 
 module.exports = {
   reviewerListing,
   coach_resume_reviews,
   seeker_resume_reviews,
-  __resolveReference
 }
