24d17472b2869bf93b88fcd63f4d3c27260b4fe3
Aaron Clark <aaronpclark92@gmail.com>
2020-02-04 12:33:09 -0600

    updated server port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 
 const server = require('./server')
-const options = { port: process.env.APOLLO_PORT || 4000 }
+const options = { port: (process.env.APOLLO_PORT || process.env.PORT) || 4000 }
 
 server.start(options, ({ port }) => {
     console.log(`QualityHub -- ResumeQ is running on ${port}`)
